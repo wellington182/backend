aded9b00dfb0e7a1c6c56934622715dbe1e0827f
Wellington Fernando da Silva <prico182@gmail.com>
2021-02-09 18:56:35 -0300

    Inclusão conexão com banco de dados mysql

--- config/app.js
+++ config/app.js
@@ -1,5 +1,20 @@
+
+var mysql = require('mysql');
+var connectionString = 'mysql://root:prico182@localhost/empresa';
+
+var db = {};
+db.cnn = {};
+db.cnn.exec = function( query, callback ) {
+    var connection = mysql.createConnection( connectionString );
+    connection.query( query, function( err, rows) {
+        callback( rows, err );
+        connection.end();
+    } );
+};
+
 var App = {
-    FILE_BASE: 'dados/base-de-dados.txt'
-}
+    FILE_BASE: 'dados/base-de-dados.txt',
+    db: db
+};
 
 module.exports = App;
